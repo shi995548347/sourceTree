acd07cac7c5d90d449f0bc2d7c5765c11252544a
web/lbb <935903022@qq.com>
2017-03-15 13:44:35 +0800

    article_detail api end

--- js/article_detail.js
+++ js/article_detail.js
@@ -2,7 +2,22 @@
  * Created by Administrator on 2017/3/15.
  */
 $(function(){
-
+    $.ajax({
+        type: "get",
+        dataType: "JSONP",
+        url: 'http://api2.shanhecang.com/article_detail',
+        data:{article_id:'37'},
+        success:function (msg) {
+            console.log(msg.data);
+            $('.des_words').html(msg.data.article_content);
+            $('.author_head').attr('src','http://image.shanhecang.com/' + msg.data.user_image);
+            $('.comment_count').html(msg.data.article_comment_count);
+            $('.like_count').html(msg.data.article_liked_count);
+            for(var i=0;i<msg.data.article_image_all.length;i++){
+                $('.article_img').attr('src','http://image.shanhecang.com/'+ msg.data.article_image_all[i].filename)
+            }
+        }
+    });
     pushHistory();
     window.addEventListener("popstate", function(e) {
         //alert("我监听到了浏览器的返回按钮事件啦");//根据自己的需求实现自己的功能
