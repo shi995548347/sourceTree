2f2fe858b68defd4bd81e45614d0f87dcebab684
web/lbb <935903022@qq.com>
2017-03-16 08:46:12 +0800

    creat login.js

--- js/login.js
+++ js/login.js
@@ -2,6 +2,14 @@ $(function(){
 	clickInput();
 })
 function clickInput(){
+	var username=$('#username');
+	var password=$('#password');
+	var login=$('#login');
+    login.click(function () {
+
+    });
+
+
 	$("form input").click(function(){
 		$(this).siblings("input").removeClass("click_input");
 		$(this).addClass("click_input");
