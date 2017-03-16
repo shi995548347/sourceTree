45295cb12e892cc1562f9b86690ecfe1a1a1898b
web/lbb <935903022@qq.com>
2017-03-16 20:07:46 +0800

    forget_pwd api not through

--- js/forget_pwd.js
+++ js/forget_pwd.js
@@ -2,23 +2,16 @@
  * Created by Administrator on 2017/3/16.
  */
 $(function () {
-    checkInput();
-});
-function checkInput() {
     var InterValObj; //timer变量，控制时间
     var count = 60; //间隔函数
     var curCount;//当前剩余秒数
-    var code = ""; //验证码
     var ajaxData = "";
-
-
 
     $('#btnSendCode').click(function() {
         curCount = count;
 
-        var phone = $("#phone").val();//手机号码
+        var phone = $("#phone").val();
         var captcha = $("#captcha").val();
-        var password = $(".set_pwd").val();
 
         //判断手机号码是否为空
         if(phone!=''){
@@ -30,9 +23,18 @@ function checkInput() {
             //向后台发送处理数据
             $.ajax({
                 type: "get",
-                dataType: "JSON",
-                url: 'http://api2.shanhecang.com/sms/phone/'+phone+'/type/1',
+                dataType: "JSONP",
+                url: 'http://api2.shanhecang.com/sms/phone/'+phone+'/type/3',
                 success: function (msg){
+                    console.log(msg);
+                    //if(msg.code==1004){
+                    //    alert('手机号码没注册，请检查后');
+                    //}else if(msg.code==1005){
+                    //    clearInterval(InterValObj);
+                    //    alert('手机短信发送失败，请重新点击获取');
+                    //    $("#btnSendCode").attr("disabled", "false");
+                    //    $("#btnSendCode").val('获取验证码');
+                    //}
                     ajaxData = msg;
                 },
                 error: function (XMLHttpRequest, textStatus, errorThrown) { },
@@ -43,25 +45,56 @@ function checkInput() {
 
     });
 
-
-
+    //$('#determine').click(function(){
+    //    var captcha = $("#captcha").val();
+    //    var password = $("#new_pwd").val();
+    //    var phone = $("#phone").val();
+//
+    //    $.ajax({
+    //        type: "post",
+    //        url: 'http://api2.shanhecang.com/passwordf',
+    //        data: {
+    //            phone: phone,
+    //            captcha:captcha,
+    //            type: '3',
+    //            client:'wap',
+    //            password:password
+    //        },
+    //        success: function (msg) {
+    //            console.log(msg);
+    //            //if(msg.status==1){
+    //            //    console.log(msg);
+    //            //    window.location.href="login.html";
+    //            //}else if(msg.code==1001){
+    //            //    alert('手机号输入错误');
+    //            //}else if(msg.code==1006){
+    //            //    alert('验证码过期，请重新获取');
+    //            //}
+    //        },
+    //        error: function (data) {
+    //            //console.log(data);
+    //        }
+    //    });
+//
+    //});
 
     $('#determine').click(function(){
         var captcha = $("#captcha").val();
-        var password = $(".set_pwd").val();
+        var password = $("#new_pwd").val();
         var phone = $("#phone").val();
         $.ajax({
             type: "post",
             dataType: "JSON",
-            url: 'http://api2.shanhecang.com/register',
+            url: 'http://api2.shanhecang.com/passwordf',
             data: {
                 phone: phone,
-                type: '1',
+                type: '3',
                 client:'wap',
                 password:password,
                 captcha:captcha
             },
             success: function (msg) {
+                console.log(msg);
                 if(msg.status==1){
                     window.location.href="../index.html";
                 }else if(msg.code==1001){
@@ -72,13 +105,14 @@ function checkInput() {
                     alert('验证码错误，请重新获取');
                 }
             },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
+            error: function (XMLHttpRequest, textStatus, errorThrown,msg) {
+                console.log(msg)
             }
         });
 
     });
 
-    //timer处理函数
+
     function SetRemainTime() {
         if (curCount == 0) {
             window.clearInterval(InterValObj);//停止计时器
@@ -91,4 +125,4 @@ function checkInput() {
             $("#btnSendCode").val(curCount + "S");
         }
     }
-}
+});
