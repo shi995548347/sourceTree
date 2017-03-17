cee108e8d7d9f7624d2b0866aa96b9a1f6fcbbd2
web/lbb <935903022@qq.com>
2017-03-17 17:34:34 +0800

    change article_detail img

--- js/article_detail.js
+++ js/article_detail.js
@@ -3,28 +3,7 @@
  */
 $(function(){
 
-    var article_content=$('.des_words');
-    var user_image=$('.author_head');
-    var article_comment_count=$('.comment_count');
-    var article_liked_count=$('.like_count');
-    var article_img=$('.article_img');
 
-    $.ajax({
-        type: "get",
-        dataType: "JSONP",
-        url: 'http://api2.shanhecang.com/article_detail',
-        data:{article_id:'37'},
-        success:function (msg) {
-            //console.log(msg.data);
-            article_content.html(msg.data.article_content);
-            user_image.attr('src','http://image.shanhecang.com/' + msg.data.user_image);
-            article_comment_count.html(msg.data.article_comment_count);
-            article_liked_count.html(msg.data.article_liked_count);
-            for(var i=0;i<msg.data.article_image_all.length;i++){
-                article_img.attr('src','http://image.shanhecang.com/'+ msg.data.article_image_all[i].filename)
-            }
-        }
-    });
 
     var parent_message=$('.parent_comment_content');
     var comment_count=$('.count');
