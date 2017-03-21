829fa4bc19f5dde0834c9c9d9ed579449608c4c0
web/lbb <935903022@qq.com>
2017-03-21 13:58:14 +0800

    active_sheet css and api

---
+++ js/active_sheet.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Administrator on 2017/3/21.
+ */
+$(function () {
+    function formatDate(now) {
+        var year = now.getFullYear();
+        var month = now.getMonth() + 1;
+        var date = now.getDate();
+        return " " + year + "年" + month + "月" + date + "日";
+    }
+    var active_time=$('.active_time');
+    var active_title=$('.active_title');
+    $.ajax({
+        type:'get',
+        dataType:'jsonp',
+        data:{activity_id:20},
+        url:'http://api2.shanhecang.com/promotion_detail',
+        success:function (msg) {
+            console.log(msg);
+            var activity_data = new Date(Number(msg.data.activity_end_date)*1000);
+            active_time.html(formatDate(activity_data));
+            active_title.html(msg.data.activity_title);
+        }
+    })
+});
